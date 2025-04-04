eb00df15a5cc021994ef5c3a7af1638b6e53e028
Michael Mior <mmior@mail.rit.edu>
2025-04-04 11:41:03 -0400

    Don't use Node.js import protocol

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from "node:fs";
+import { readFileSync } from "fs";
 import { program } from "commander";
 
 import { applyLensToDoc } from "./doc";
--- src/json-schema.ts
+++ src/json-schema.ts
@@ -1,4 +1,4 @@
-import { inspect } from "node:util";
+import { inspect } from "util";
 import type {
   JSONSchema7,
   JSONSchema7Definition,
