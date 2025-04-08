69ce558bf264488428c72b9f0401e4cb31688da5
Michael Mior <mmior@mail.rit.edu>
2025-04-08 13:05:58 -0400

    Add tests with Playwright

---
+++ e2e/demo.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "@playwright/test";
+
+test("has input set correctly", async ({ page }) => {
+  await page.goto("/demo/index.html");
+
+  await expect(
+    page.locator("cambria-document").first().getByLabel("root[name]"),
+  ).toHaveValue(/The Fifth Element/);
+});
